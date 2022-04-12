ae8837e4f9a972185a9430f932f7b88c653aa4dd
Cenk Kılıç <cenk@kilic.dev>
2022-04-12 23:42:37 +0200

    update connfiguration for ssh generation

--- hostfs/etc/cont-init.d/30-generate-keypairs.sh
+++ hostfs/etc/cont-init.d/30-generate-keypairs.sh
@@ -5,6 +5,8 @@ source /scripts/logger.sh
 if [ ! -f /home/service/.ssh/id_rsa_1 ]; then
 	log_start "Generating SSH keypairs..." "top"
 
+	mkdir -p /home/service/.ssh
+
 	ssh-keygen -t rsa -b 4096 -C "$HOSTNAME" -f /home/service/.ssh/id_rsa_1
 
 	log_warn "This is a one time operation. Please mount a persistent volume to /conf to keep this configuration!"
