a211c86537487dcfb78dde96ebc45f8c2dc0277d
Cenk Kılıç <cenk@kilic.dev>
2022-04-13 00:07:45 +0200

    chore: update error message

--- hostfs/etc/cont-init.d/30-generate-keypairs.sh
+++ hostfs/etc/cont-init.d/30-generate-keypairs.sh
@@ -9,7 +9,7 @@ if [ ! -f /home/service/.ssh/id_rsa_1 ]; then
 
 	ssh-keygen -t rsa -b 4096 -C "$HOSTNAME" -f /home/service/.ssh/id_rsa_1
 
-	log_warn "This is a one time operation. Please mount a persistent volume to /conf to keep this configuration!"
+	log_warn "This is a one time operation. Please mount a persistent volume to /home/service/ to keep this configuration!"
 fi
 
 chown service:service -R /home/service/.ssh/
